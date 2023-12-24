98feb669c143a4dd65fb63df596cc71fe5707c89
Umair Ahmed <86409567+ahmedumair1122@users.noreply.github.com>
2023-12-24 22:56:46 +0500

    send email and verify otp

--- Src/Screens/ForgetPassword/VerifyOtp.js
+++ Src/Screens/ForgetPassword/VerifyOtp.js
@@ -2,24 +2,24 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image, Alert } from 'react-native';
 import axios from 'axios';
 
-const VerifyOtp = ({changeScreen}) => {
+const VerifyOtp = ({ changeScreen }) => {
   const [otp, setOtp] = useState(''); // State to store OTP
   const [otpError, setOtpError] = useState('');
+  const [verificationError, setVerificationError] = useState('');
 
   const handleVerify = async () => {
     // Validate OTP
-    if (!otp.trim()) {
-      setOtpError('Enter the 6-digit OTP');
+    if (!otp.trim() || otp.length !== 6) {
+      setOtpError('Enter a valid 6-digit OTP');
       return;
     }
 
     try {
       const apiUrl = 'http://182.176.169.225:19008/api/v1/users/verifyCode';
 
-      const response = await axios.post(apiUrl, { otp }, {
+      const response = await axios.put(apiUrl, { otp }, {
         headers: {
           'Content-Type': 'application/json',
-          // Add any other required headers here
         },
       });
 
@@ -28,21 +28,20 @@ const VerifyOtp = ({changeScreen}) => {
       if (response.status === 200) {
         console.log('OTP verified successfully');
         // Handle success, maybe navigate to another screen
-        // navigation.navigate('PasswordResetScreen');
       } else {
         console.error('Failed to verify OTP');
-        Alert.alert('Verification Failed', 'Invalid OTP. Please try again.');
+        setVerificationError('Invalid OTP. Please try again.');
       }
     } catch (error) {
-      changeScreen('resetPassword')
       console.error('Error occurred while verifying OTP:', error);
-      Alert.alert('Error', 'An error occurred while verifying OTP. Please try again.');
+      setVerificationError('An error occurred while verifying OTP. Please try again.');
     }
   };
 
   const handleOtpChange = text => {
     setOtp(text);
     setOtpError('');
+    setVerificationError(''); // Clear verification error when OTP changes
   };
 
   return (
@@ -51,7 +50,7 @@ const VerifyOtp = ({changeScreen}) => {
         <Image
           style={{ margin: 20 }}
           source={require('../../Assets/Icons/Info.png')}
-        ></Image>
+        />
         <Text
           numberOfLines={2}
           style={{ color: '#F0B212', fontSize: 12, width: '75%' }}>
@@ -61,24 +60,22 @@ const VerifyOtp = ({changeScreen}) => {
       </View>
       <View style={{ width: '80%' }}>
         <TextInput
-          style={styles.TextInput}
+          style={styles.textInput}
           keyboardType="numeric"
           placeholderTextColor={'#9EA1AE'}
-          placeholder="Enter 6 digit OTP"
+          placeholder="Enter 6-digit OTP"
           value={otp}
           onChangeText={handleOtpChange}
         />
         {otpError ? <Text style={styles.errorMessage}>{otpError}</Text> : null}
+        {verificationError ? <Text style={styles.errorMessage}>{verificationError}</Text> : null}
       </View>
-      <TouchableOpacity style={styles.LoginButton} onPress={handleVerify}>
+      <TouchableOpacity style={styles.loginButton} onPress={handleVerify}>
         <Text style={{ color: '#FFFF', fontSize: 14 }}>Verify</Text>
       </TouchableOpacity>
     </View>
   );
 };
-
-export default VerifyOtp;
-
 
 const styles = StyleSheet.create({
   verifyscreenContainer: {
@@ -96,7 +93,7 @@ const styles = StyleSheet.create({
     alignContent: 'center',
     justifyContent: 'space-between',
   },
-  LoginButton: {
+  loginButton: {
     backgroundColor: '#384DAA',
     height: 59,
     width: '80%',
@@ -104,12 +101,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  TextInput: {
+  textInput: {
     borderWidth: 1,
     borderColor: '#F3F4F9',
     padding: 10,
     borderRadius: 30,
-    width: '100%', // Modified to take up the full width
+    width: '100%',
     color: '#090D20',
   },
   errorMessage: {
@@ -118,3 +115,5 @@ const styles = StyleSheet.create({
     marginTop: 5,
   },
 });
+
+export default VerifyOtp;
